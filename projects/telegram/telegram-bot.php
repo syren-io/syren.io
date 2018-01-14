1dde714b9a6438a61d63248b366e510ce50ff03f
EC2 Default User <admin@syren.io>
2018-01-14 00:12:43 +0000

    test

--- projects/telegram/telegram-bot.php
+++ projects/telegram/telegram-bot.php
@@ -4,5 +4,10 @@ $token = "449164880:AAFUdtUiVzI7rP6W49pjyVvMjmFPM5Ef2pg";
 
 $response = file_get_contents("https://api.telegram.org/bot{$token}/getMe");
 
-echo json_decode($response);
+$response = json_decode($response);
+
+echo "{$response->result->username} was here!";
+echo "<pre>";
+print_r($response);
+echo "</pre>";
 
